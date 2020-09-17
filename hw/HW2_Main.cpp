ae991ea4fa96dd53810e79ccb581ad0facdef60c
Roman Bellisari <32945706+romanbell@users.noreply.github.com>
2020-09-17 12:21:18 -0400

    Add files via upload

---
+++ hw/HW2_Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+/*
+int lcm(int a, int b) {
+}
+*/
+
+int powermod(int base, int exp, int mod) {
+    if (mod == 1) return 0;
+    int prod = 1;
+    base = base % mod;
+    while (exp > 0) {
+        if (exp&1) prod = (prod * base) % mod;
+        base = (base * base) % mod;
+        exp = exp/2;
+    }
+    return prod;
+}
+
+int main() {
+    std::cout << "GCD: " << gcd(36,60) << '\n'; 
+    //std::cout << "LCM: " << lcm(12,30) << '\n';
+    std::cout << "PMOD: " << powermod(4, 13, 497) << '\n';
+}
